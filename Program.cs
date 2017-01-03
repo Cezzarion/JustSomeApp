368bdc1388c75ad3c50d1ab76de6b16070645f2b
Cezzarion <andronvic@yahoo.com>
2017-01-03 12:01:07 +0300

    JustSomeApp
    Some console application to try Git Desktop

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustSomeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Another try with git!");
+
+            Console.ReadLine();
+        }
+    }
+}
